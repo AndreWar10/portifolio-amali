725969886466c7153fe8fd25152f6964e40976f1
AndreWar10 <andregs.dev@gmail.com>
2026-06-16 01:08:38 -0300

    Add Vercel configuration and clean up Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Image from "next/image";
 import { useEffect, useState } from "react";
 import { navItems, siteConfig } from "@/lib/site";
 import { MobileNav } from "./MobileNav";
